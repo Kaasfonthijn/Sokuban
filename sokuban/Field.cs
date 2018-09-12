05c947ecaddcf54c176c852900e8d60f5d99b902
Thijnlinders <thijnlinders@hotmail.nl>
2018-09-12 13:08:29 +0200

    sokuban

---
+++ sokuban/Field.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sokuban
+{
+    class Field
+    {
+        private Truck truck;
+
+        public Field()
+        {
+            truck = new Truck();
+        }
+
+        public void moveTo { get; set; }
+    }
+}
